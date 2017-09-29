d872bf10b9be5a0cd45a4ebf3bf3121b0b4c9c61
Maxwell Lasky <mlasky46@gmail.com>
2017-09-29 14:23:02 -0600

    built my own router

--- subjects/MiniRouter/exercise/mini-router.js
+++ subjects/MiniRouter/exercise/mini-router.js
@@ -18,31 +18,49 @@ history.push('/something')
 
 class Router extends React.Component {
   history = createHashHistory()
+  listener = this.history.listen((history) => {
+    this.history.location.pathname = history.pathname
+    this.forceUpdate()
+  })
+
+  static childContextTypes = {
+    history: PropTypes.object.isRequired
+  }
+
+  getChildContext() {
+    return {
+      history: this.history
+    }
+  }
 
   render() {
     return this.props.children
   }
 }
 
-
-
 class Route extends React.Component {
+  static contextTypes = {
+    history: PropTypes.object.isRequired
+  }
+
   render() {
-    const { path, render, component:Component } = this.props
+    const { path, render, component: Component } = this.props
+    const { history } = this.context
+    if (history.location.pathname === path && render) return render()
+    if (history.location.pathname === path && this.props.component) return <Component />
     return null
   }
 }
 
-
-
-
 class Link extends React.Component {
+  static contextTypes = {
+    history: PropTypes.object.isRequired
+  }
 
   handleClick = (e) => {
     e.preventDefault()
-
+    this.context.history.push(this.props.to)
   }
-
   render() {
     return (
       <a
